79ea8fcd224f71030b8d21b173ea5ff4a6d4a9d4
Troy Weaver <troy.weaver09@gmail.com>
2025-12-14 06:29:49 -0800

    Handle empty race results when getting latest race date
    Prevent exception by checking for race results before calling Max.
    Set latestResultsRaceDate to DateTime.Now if no results exist.

--- Projects/WebApp/Pages/Dashboard.cshtml.cs
+++ Projects/WebApp/Pages/Dashboard.cshtml.cs
@@ -78,7 +78,11 @@ namespace WebApp.Pages
             OverallPlace = standings.FindIndex(s => s.UserId == UserId) + 1;
             TotalPoints = standings.FirstOrDefault(s => s.UserId == UserId)?.TotalPoints ?? 0;
 
-            var latestResultsRaceDate = _context.RaceResults.Max(rr => rr.Race.Date);
+            DateTime latestResultsRaceDate = DateTime.Now;
+            if (_context.RaceResults.Any())
+            {
+                latestResultsRaceDate = _context.RaceResults.Max(rr => rr.Race.Date);
+            }
 
             var recentRace = await _context.Races
                 .Where(r => r.Pool.Id == currentSeason.Id && r.Date <= latestResultsRaceDate)
